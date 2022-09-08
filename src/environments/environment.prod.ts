8ff5e81807f1a251d49a2edf528c9c89757bb51d
RogoJuerg <juergen.rogozin@gmail.com>
2022-09-08 07:23:48 +0200

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'login-1f3ed',
+    appId: '1:106742356128:web:6ff155f1fb6925936351c5',
+    storageBucket: 'login-1f3ed.appspot.com',
+    apiKey: 'AIzaSyCncHXZtKH3ADMr9ZjiC3_LBPaAe-Rejwc',
+    authDomain: 'login-1f3ed.firebaseapp.com',
+    messagingSenderId: '106742356128',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'login-1f3ed',
+    appId: '1:106742356128:web:6ff155f1fb6925936351c5',
+    storageBucket: 'login-1f3ed.appspot.com',
+    apiKey: 'AIzaSyCncHXZtKH3ADMr9ZjiC3_LBPaAe-Rejwc',
+    authDomain: 'login-1f3ed.firebaseapp.com',
+    messagingSenderId: '106742356128',
+  },
   production: false
 };
 
